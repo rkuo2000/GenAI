7032d994f7e53a127d574a90f3e5e40c8b4bd7a4
Richard Kuo <rkuo2000@gmail.com>
2024-03-16 00:00:34 +0800

    Add files via upload

---
+++ Audio-to-Text/whisper_server.py
@@ -0,0 +1,28 @@
+# pip install git+https://github.com/openai/whisper.git
+## To run server: python whisper_server.py
+## To run client: python post_audio.py
+
+from flask import Flask, request, jsonify
+import whisper
+model = whisper.load_model("base")
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "hello"
+
+@app.route("/audio", methods=['POST'])
+def audio():
+    if request.method == 'POST':
+        audiofile = request.files['audio'] 
+        print(audiofile.filename)
+        #audiofile.save(audiofile.filename)
+
+        # transcribe
+        result = model.transcribe(audiofile.filename) 
+        print(result["text"])
+        return jsonify(result["text"])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
