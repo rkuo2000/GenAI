f4e1b0cfda048b4f7d0761ba2889dd9c9eb764be
Richard Kuo <rkuo2000@gmail.com>
2024-05-28 06:00:07 +0800

    Add files via upload

--- Image-to-Text/post_imgtxt.py
+++ Image-to-Text/post_imgtxt.py
@@ -4,18 +4,20 @@
 import sys
 import requests
 
-filename = sys.argv[1]
 url = "http://127.0.0.1:5000/imgtxt/"
 
-#prompt = "USER: <image>\nWhat are the things I should be cautious about when I visit this place?\nASSISTANT:"
-#image = requests.get("https://llava-vl.github.io/static/images/view.jpg", stream=True).raw
+if len(sys.argv) < 2:
+    image = requests.get("https://llava-vl.github.io/static/images/view.jpg", stream=True).raw
+else:
+    filename = sys.argv[1]
+    image = open(filename, "rb")
 
+#prompt = "USER: <image>\nWhat are the things I should be cautious about when I visit this place?\nASSISTANT:"
 #prompt = "USER: <image>\n這是什麼?\nASSISTANT:"
 #prompt = "USER: <image>\n這是我家的什麼場所?\nASSISTANT:"
 #prompt = "USER: <image>\nWhat is this place?\nASSISTANT:"
-prompt = "USER: <image>\nWhat is the place in my home?\nASSISTANT:"
-
-image = open(filename, "rb")
+prompt = "What is the place in my home?"
+#prompt = "這是我家的什麼場所?"
 
 my_img = {'image': image }
 my_txt = {'text' : prompt}
