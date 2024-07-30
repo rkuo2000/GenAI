c53ef2961fe3d2f8b779fa739eeef9b3e61651b8
Richard Kuo <rkuo2000@gmail.com>
2024-07-31 05:50:43 +0800

    Add files via upload

---
+++ Text-to-Text/ollama_speak.py
@@ -0,0 +1,27 @@
+# sudo apt install mpg123
+# pip install ollama
+# pip install gtts
+
+import ollama
+from gtts import gTTS
+import os
+
+sl = "en"
+
+stream = ollama.chat(
+    model='tinyllama',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
+
+text = ""
+for chunk in stream:
+   content = chunk['message']['content']
+   if content!=".":
+       text = text + " " + content
+   else:
+       print(text)
+       tts = gTTS(text, lang=sl)
+       tts.save("gTTS.mp3")
+       os.system("mpg123 gTTS.mp3")
+       text = ""
