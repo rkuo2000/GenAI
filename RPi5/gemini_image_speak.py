66d4bdf533f1fdc5bddba404a4ad5bc29c7da3d6
Richard Kuo <rkuo2000@gmail.com>
2024-08-05 09:18:45 +0800

    Add files via upload

---
+++ RPi5/gemini_image_speak.py
@@ -0,0 +1,24 @@
+# pip install google.generativeai
+
+import google.generativeai as genai
+import PIL.Image
+from gtts import gTTS
+import os
+
+GOOGLE_API_KEY="get_api_key_from" ## https://aistudio.google.com/app/apikey
+genai.configure(api_key=GOOGLE_API_KEY)
+
+img = PIL.Image.open("images/test.jpg")
+prompt = "Can you tell me about this photo? simple description in traditional chinese"
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+result = model.generate_content( [prompt , img] )
+print(result.text)
+
+## TTS
+tts = gTTS(result.text, lang="zh-TW")
+tts.save("gTTS.mp3")
+
+## Speak
+os.system("mpg123 -q gTTS.mp3")
