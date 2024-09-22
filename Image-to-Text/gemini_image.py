f419df0fff230bc42ab9569975894fcfa6540c79
Richard Kuo <rkuo2000@gmail.com>
2024-09-23 03:00:26 +0800

    Update gemini_image.py

--- Image-to-Text/gemini_image.py
+++ Image-to-Text/gemini_image.py
@@ -8,7 +8,8 @@ GOOGLE_API_KEY="get_it_from" ## https://aistudio.google.com/app/apikey
 genai.configure(api_key=GOOGLE_API_KEY)
 
 img = PIL.Image.open("images/test.jpg")
-prompt = "Can you tell me about this photo?"
+#prompt = "Can you tell me about this photo?"
+prompt = "你在這張照片看到了什麼?"
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
