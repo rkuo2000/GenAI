91fea7eef80ffa5d78f7c2ea35aa8ca0612e3653
Richard Kuo <rkuo2000@gmail.com>
2024-10-10 15:55:35 +0800

    Update whisper-large-v3.py

--- Audio-to-Text/whisper-large-v3.py
+++ Audio-to-Text/whisper-large-v3.py
@@ -10,7 +10,8 @@ from datasets import load_dataset
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "openai/whisper-large-v3"
+#model_id = "openai/whisper-large-v3"
+model_id = "openai/whisper-large-v3-turbo"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained( model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True)
 
