999ecafd80b34bae02ce8286023d12686e0abedf
Richard Kuo <rkuo2000@gmail.com>
2024-10-16 06:23:31 +0800

    Update flux.1-dev.py

--- Text-to-Image/flux.1-dev.py
+++ Text-to-Image/flux.1-dev.py
@@ -2,7 +2,7 @@ import torch
 from diffusers import FluxPipeline
 
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
-#pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
 prompt = "A cat holding a sign that says hello world"
 image = pipe(
