ac16a937e02c57d7b91dafa41d226bca66998671
Richard Kuo <rkuo2000@gmail.com>
2025-02-14 01:51:43 +0800

    Add files via upload

---
+++ RPi5/yolo11_cam_speak.py
@@ -0,0 +1,58 @@
+#!/home/user/venv/bin/python
+## pip install ultralytics
+## pip install deep-translator
+## pip install gtts
+
+import os
+import cv2
+from ultralytics import YOLO
+from deep_translator import GoogleTranslator
+from gtts import gTTS
+
+model = YOLO("yolo11n.pt")
+
+def Speak(text,tl):
+    # Translate
+    text = GoogleTranslator(source="auto", target=tl).translate(text=text)
+    print(text)
+    ## TTS
+    tts = gTTS(text, lang=tl)
+    tts.save("gTTS.mp3")
+    ## Speak
+    os.system("mpg123 -q gTTS.mp3")
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    #frame = cv2.flip(frame, 1) # mirror
+
+    ## Object Detection 
+    results = model(frame)
+
+    ## Object Counting
+    labels = results[0].names
+    cls = results[0].boxes.cls.tolist()
+    unique = list(dict.fromkeys(cls))
+
+    sl = "en"
+    text = "There are "
+    for label in unique:
+        count = cls.count(label)
+        text = text + str(count) + " " + labels[int(label)] + ","
+    #print(text)
+    Speak(text, "zh-TW")
+
+    #results[0].save("out.jpg")
+    #img = cv2.imread("out.jpg")
+    #cv2.imshow('webcam', img)
+
+    #k = cv2.waitKey(1) & 0xFF
+    #if k==ord('s'):
+    #    cv2.imwrite("detected.jpg", img)
+    #    #Speak(text,sl)
+    #    Speak(text, "zh-TW")
+    #if k==27:
+    #    break
+
+cap.release()
+cv2.destroyAllWindows()
