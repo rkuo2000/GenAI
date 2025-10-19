5bf888ce14967f8f2dcb5518788e4885ac1157da
Richard Kuo <rkuo2000@gmail.com>
2025-10-20 02:58:39 +0800

    Delete Text-to-Text/gpt4free.py

--- Text-to-Text/gpt4free.py
+++
@@ -1,9 +0,0 @@
-# !pip install -U g4f
-from g4f.client import Client
-
-client = Client()
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Hello"}],
-)
-print(response.choices[0].message.content)
