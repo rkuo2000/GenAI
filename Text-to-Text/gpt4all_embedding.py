826bd9f4c48828d64f480cfd5b0f064050c97f22
Richard Kuo <rkuo2000@gmail.com>
2025-10-20 02:58:18 +0800

    Delete Text-to-Text/gpt4all_embedding.py

--- Text-to-Text/gpt4all_embedding.py
+++
@@ -1,5 +0,0 @@
-from gpt4all import Embed4All
-text = 'Who is Laurens van der Maaten?'
-embedder = Embed4All('nomic-embed-text-v1.f16.gguf')
-output = embedder.embed(text, prefix='search_query')
-print(output)
