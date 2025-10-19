851d53b202fba1a439ac401132bb6cc5983523fd
Richard Kuo <rkuo2000@gmail.com>
2025-10-20 02:58:29 +0800

    Delete Text-to-Text/gpt4all_prompting.py

--- Text-to-Text/gpt4all_prompting.py
+++
@@ -1,6 +0,0 @@
-# pip install gpt4all
-from gpt4all import GPT4All
-model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-prompt = "why is the sky blue?"
-output = model.generate(prompt, max_tokens=512)
-print(output)
