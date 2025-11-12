7e02eed72edb621b3830cdaa524a8cf29a57cf1d
Richard Kuo <rkuo2000@gmail.com>
2025-11-13 00:45:36 +0800

    Add files via upload

---
+++ Audio-to-Text/omnilingual.py
@@ -0,0 +1,8 @@
+from omnilingual_asr.models.inference.pipeline import ASRInferencePipeline
+
+pipeline = ASRInferencePipeline(model_card="omniASR_LLM_3B")
+
+audio_files = ["./audio/Lai.mp3", "./audio/Trump.mp3"]
+lang = ["cmn_Hant", "eng_Latn"]
+transcriptions = pipeline.transcribe(audio_files, lang=lang, batch_size=2)
+print(transcriptions)
