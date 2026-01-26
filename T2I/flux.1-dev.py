db38c01657168dc2c179aba772680c3b0f6bb0e3
Richard Kuo <rkuo2000@gmail.com>
2026-01-26 20:41:39 +0800

    Add files via upload

---
+++ T2I/flux.1-dev.py
@@ -0,0 +1,17 @@
+import torch
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+
+prompt = "A cat holding a sign that says hello world"
+image = pipe(
+    prompt,
+    height=1024,
+    width=1024,
+    guidance_scale=3.5,
+    num_inference_steps=50,
+    max_sequence_length=512,
+    generator=torch.Generator("cpu").manual_seed(0)
+).images[0]
+image.save("flux-dev.png")
---
+++ T2I/sdxl-lightning-lora.py
@@ -0,0 +1,23 @@
+# pip install diffusers accelerate
+import torch
+from diffusers import StableDiffusionXLPipeline, EulerDiscreteScheduler
+from huggingface_hub import hf_hub_download
+
+base = "stabilityai/stable-diffusion-xl-base-1.0"
+repo = "ByteDance/SDXL-Lightning"
+ckpt = "sdxl_lightning_4step_lora.safetensors" # Use the correct ckpt for your step setting!
+
+# Load model.
+pipe = StableDiffusionXLPipeline.from_pretrained(base, torch_dtype=torch.float16, variant="fp16").to("cuda")
+pipe.load_lora_weights(hf_hub_download(repo, ckpt))
+pipe.fuse_lora()
+
+# Ensure sampler uses "trailing" timesteps.
+pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+
+# Ensure using the same inference steps as the loaded model and CFG set to 0.
+image = pipe("A K-pop girl smiling on the beach", num_inference_steps=4, guidance_scale=0).images[0]
+image.save("output.png")
+image.show()
+
+
