bea09e0ca19d214208b34b3b48fcfbc664f57a35
Richard Kuo <rkuo2000@gmail.com>
2026-01-26 20:26:54 +0800

    Delete Image-to-Text directory

--- Image-to-Text/crop_mp4.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -i $1 -vf "crop=512:512:384:0" out.mp4
--- Image-to-Text/stt.py
+++
@@ -1,7 +0,0 @@
-# Transcribe gTTS.mp3 to text
-
-import whisper
-ASR = whisper.load_model("base")
-
-result = ASR.transcribe("gTTS.mp3")
-print(result["text"])
