7009c33791b28f223e38dfdbe958685cd53e25f3
Richard Kuo <rkuo2000@gmail.com>
2026-03-14 22:19:00 +0800

    Add files via upload

---
+++ VLM/gemini_image.py
@@ -0,0 +1,26 @@
+# pip install google-genai
+
+from google import genai
+import PIL.Image
+import os
+import sys
+
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+if len(sys.argv)>1:
+    filename = sys.argv[1]
+else:
+#    filename = "images/test_big_data.png"
+     filename="images/recycle_bin.jpg"
+     
+img = PIL.Image.open(filename)
+
+#prompt = "Can you tell me about this photo?" + "please answer in traditional chinese"
+#prompt = "請問前三名跟佳作總共可獲得多少錢?"
+prompt = "此為紙容器回收桶，請偵測非紙容器之回收物，並標示所在位置"
+response = client.models.generate_content(
+    model ="gemini-3-flash-preview",
+    contents = [prompt , img]
+)
+print(response.text)
