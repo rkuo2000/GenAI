ab154ce576c0525f4697f8fac8b4a79a1eddfb4e
Richard Kuo <rkuo2000@gmail.com>
2026-06-13 05:42:30 +0800

    Delete LLM/ollama_openai.sh

--- LLM/ollama_openai.sh
+++
@@ -1,8 +0,0 @@
-#curl http://localhost:11434/api/chat -d '{
-curl http://localhost:11434/v1/chat/completions -d '{
-  "model": "gemma4:e2b",
-  "messages": [{
-    "role": "user",
-    "content": "Why is the sky blue?"
-  }],
-}'
